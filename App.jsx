38a3050bf65c88eebb956ce671f2116cab38d8b8
Lê Văn Toàn <vantoan2509@gmail.com>
2022-05-16 15:19:09 +0700

    done

--- App.jsx
+++ App.jsx
@@ -1,37 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Map4dMap from './Map4dMap.jsx'
 
-class App extends React.Component {
-  render() {
-    return (
-      <div style={{ width: '100%', height: '400px' }}>
-        <Map4dMap
-          style={{ width: '100%', height: '200px' }}
-          id={'id1'}
-          onMapReady={map => console.log(map)}
-          options={{
-            center: { lat: 16.072163491469226, lng: 108.22690536081757 },
-            zoom: 15,
-            controls: true
-          }}
-          accessKey="YOUR_ACCESS_KEY"
-          version="2.2" />
-          
-        <Map4dMap
-          style={{ width: '100%', height: '200px' }}
-          mapid = "YOUR_MAP_ID"
-          id={'id2'}
-          onMapReady={map => console.log(map)}
-          options={{
-            center: { lat: 16.072163491469226, lng: 108.22690536081757 },
-            zoom: 15,
-            controls: true
-          }}
-          accessKey="YOUR_ACCESS_KEY"
-          version="2.2" />
-      </div>
-    );
+function App() {
+  const [maps, setMaps] = useState([])
+  const [version, setVersion] = useState("")
+  const [key, setKey] = useState("")
+  const [id, setId] = useState("")
+  const changeVersion = (e) => {
+    let newValue = e.target.value
+    setVersion(newValue)
   }
+  const changeKey = (e) => {
+    let newValue = e.target.value
+    setKey(newValue)
+  }
+  const changeId = (e) => {
+    let newValue = e.target.value
+    setId(newValue)
+  }
+  const add = () => {
+    setMaps(prev => {
+      let newMaps = [...prev]
+      newMaps.push({
+        key: key,
+        id: id,
+        version: version
+      })
+      return newMaps
+    })
+  }
+  const remove = () => {
+    setMaps(prev => {
+      let newMaps = [...prev]
+      newMaps.pop()
+      return newMaps
+    })
+  }
+  return (
+    <div style={{ width: '100%', height: '400px' }}>
+      <input placeholder='version' onChange={changeVersion} />
+      <input placeholder='key' onChange={changeKey} />
+      <input placeholder='id' onChange={changeId} />
+      <button onClick={add}>Thêm map</button>
+      <button onClick={remove}>Xóa map cuối</button>
+      {
+        maps.map(map => {
+          return (
+            <Map4dMap
+              key={map.id}
+              style={{ width: '100%', height: '200px' }}
+              id={map.id}
+              onMapReady={map => console.log(map)}
+              options={{
+                center: { lat: 16.072163491469226, lng: 108.22690536081757 },
+                zoom: 15,
+                controls: true
+              }}
+              accessKey={map.key}
+              version={map.version} />
+          )
+        })
+      }
+
+
+      <Map4dMap
+        style={{ width: '100%', height: '200px' }}
+        mapid="YOUR_MAP_ID"
+        id={'id2'}
+        onMapReady={map => console.log(map)}
+        options={{
+          center: { lat: 16.072163491469226, lng: 108.22690536081757 },
+          zoom: 15,
+          controls: true
+        }}
+        accessKey="YOUR_ACCESS_KEY"
+        version="2.2" />
+    </div>
+  );
 }
 
 export default App;
